1e8eee932b62cf7d6878ce3886dc1aa972be3d53
Sharon Aicler <saichler@gmail.com>
2023-11-14 06:43:50 -0800

    add accept

--- go/sec_common/security_interfaces.go
+++ go/sec_common/security_interfaces.go
@@ -4,6 +4,7 @@ import "net"
 
 type SecurityProvider interface {
 	CanDial(string, uint32, ...interface{}) (net.Conn, error)
+	CanAccept(conn net.Conn) error
 	ValidateConnection(net.Conn, string, ...interface{}) (string, error)
 	Encrypt([]byte) (string, error)
 	Decrypt(string) ([]byte, error)
