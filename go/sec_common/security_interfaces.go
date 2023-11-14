71afddb9f05e90dd4ad839bea8f2ce7d29ecdc65
Sharon Aicler <saichler@gmail.com>
2023-11-14 06:21:51 -0800

    Alter Sec provider

--- go/sec_common/security_interfaces.go
+++ go/sec_common/security_interfaces.go
@@ -1,12 +1,12 @@
 package sec_common
 
+import "net"
+
 type SecurityProvider interface {
-	CanConnectFrom(string, string, uint32) bool
-	CanConnectTo(string, string, uint32) bool
+	CanDial(string, uint32, ...interface{}) (net.Conn, error)
+	ValidateConnection(net.Conn, string, ...interface{}) (string, error)
 	Encrypt([]byte) (string, error)
 	Decrypt(string) ([]byte, error)
-	EncryptedSecret() (string, error)
-	IsSecret(string) bool
 }
 
 var MySecurityProvider SecurityProvider
